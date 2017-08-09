93479c0b358ae2ab1d1d9133af0cbd11b4a2f505
wuarthur <yaqixyz@gmail.com>
2017-08-08 20:06:26 -0700

    more changes

--- scan_course.py
+++ scan_course.py
@@ -1,21 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import SMPT
 
 
 URLs = []
 class _CourseUrl:
     def __init__(self, dept, course, section):
         self.url= 'https://courses.students.ubc.ca/cs/main?pname=subjarea&tname=subjareas&req=5&dept=' + dept +'&course='+ course+ '&section=' + section
-
+        self.name = dept + ' ' + course + ' ' + section
 URLs.append(_CourseUrl('AANB','500','KRS'))
 URLs.append(_CourseUrl('GEM','520','101'))
-
 for course in URLs:
     r = requests.get(course.url)
     html_doc = r.text
     if 'Total Seats Remaining:</td><td align=&#39;left&#39;><strong>0' not in html_doc:
-        print 'total'
+        SMPT._sendEmailToUsers(users,course.name)
     if 'General Seats Remaining:</td><td align=&#39;left&#39;><strong>0' not in html_doc:
         print 'general'
-    if 'Restricted Seats Remaining*:</td><td align=&#39;left&#39;><strong>0' not in html_doc:
-        print 'restricted'
+

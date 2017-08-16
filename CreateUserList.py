85bb65fd03272c8b9bfda3926c7f7511e7416f12
wuarthur <yaqixyz@gmail.com>
2017-08-15 18:51:45 -0700

    removed james, added phil 120

--- CreateUserList.py
+++ CreateUserList.py
@@ -1,8 +1,8 @@
 import pickle
 
-users = {'Henry89098@gmail.com': ['Hantao_Mai',['CPSC,121,188,General','CPSC,221,101,Any']],
+users = {'Henry89098@gmail.com': ['Hantao_Mai',['CPSC,121,188,General','CPSC,221,191,Any']],
          'henry78987@gmail.com': ['Yi_Yang',['CPSC,121,181,General','CPSC,565,121,Any']],
-         'james.k.1452@hotmail.com': ['James_Kurniawan',['CPSC,310,L1D,Any']]}
+         'Yaqixyz@gmail.com': ['y',['PHIL,120,99C,Any']]}
 
 with open('users.pickle', 'wb') as handle:
     pickle.dump(users, handle, protocol=pickle.HIGHEST_PROTOCOL)

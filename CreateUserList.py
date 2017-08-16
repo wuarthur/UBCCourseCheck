2d7919a4bd21619ea8425d0263fbcdc46f7a7b11
wuarthur <yaqixyz@gmail.com>
2017-08-15 20:55:21 -0700

    add Eosc

--- CreateUserList.py
+++ CreateUserList.py
@@ -2,7 +2,7 @@ import pickle
 
 users = {'Henry89098@gmail.com': ['Hantao_Mai',['CPSC,121,101,General','CPSC,221,101,Any']],
          'henry78987@gmail.com': ['Yi_Yang',['CPSC,121,101,General','CPSC,565,121,Any']],
-         'james.k.1452@hotmail.com': ['James_Kurniawan',['CPSC,310,L1D,Any']],
+         'yaqixyz@gmail.com': ['y',['EOSC,326,99A,General']],
          'Yonni.luu@gmail.com': ['Yonni_Luu',['GERM,433,902,General']]}
 
 

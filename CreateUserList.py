231a93ad9f6a651aa7ce4cd743c266de583efd9f
wuarthur <yaqixyz@gmail.com>
2017-08-16 11:25:53 -0700

    removed cpsc 121 101

--- CreateUserList.py
+++ CreateUserList.py
@@ -1,7 +1,6 @@
 import pickle
 
-users = {'Henry89098@gmail.com': ['Hantao_Mai',['CPSC,121,101,General','CPSC,221,101,Any']],
-         'henry78987@gmail.com': ['Yi_Yang',['CPSC,121,101,General','CPSC,565,121,Any']],
+users = {'Henry89098@gmail.com': ['Hantao_Mai',['CPSC,121,999,General','CPSC,221,999,Any']],
          'yaqixyz@gmail.com': ['y',['EOSC,326,99A,General','PHIL,120,99C,Any']],
          'Yonni.luu@gmail.com': ['Yonni_Luu',['GERM,433,902,General']],
 		 'james.k.1452@hotmail.com': ['tanboi',['PHYS,219,L1A,Any']]}
